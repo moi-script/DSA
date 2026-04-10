5de0eedc773ed722328d162807719bcdd3ba8d55
lowkeyCodee <rizskplay@gmail.com>
2026-04-10 14:42:24 +0800

    infinite recusive list glitch

---
+++ recurrence/recursive.js
@@ -0,0 +1,34 @@
+
+
+let fnList = [];
+let iterationCount = 0;
+function loops(iterationCount, fnList, fnConsole) {
+    if (iterationCount === 5) {
+        console.log('Fn list result :: ', fnList)
+        return fnList;
+    }
+    iterationCount++;
+
+
+    if(typeof fnConsole === 'function') {
+        fnConsole("Iteration count :: ", iterationCount);
+    }
+    console.log('Next')
+
+    if(Array.isArray(fnList)) fnList.push(loops) 
+    
+     return loops(iterationCount, fnList, fnConsole);
+}
+
+
+function getLoops(fnList){
+    console.log("fn list first index :: ", fnList[0](0, [], console.log)[0](0, [], console.log)[0](0, [], console.log)[0](0, [], console.log)[0](0, [], console.log))
+    // fnList.forEach((fn) => {
+    //     console.log("Result :: ", fn(1, []))
+    // })
+
+}
+
+getLoops(loops(iterationCount, fnList, undefined))
+
+console.log(typeof undefined);
--- sortingAL/mergeSort.js
+++ sortingAL/mergeSort.js
@@ -1,24 +1,61 @@
 
 function merge(left, right) {
-    let result= [];
+    let result = [];
+    console.log("From merge fn :: ", left);
+    console.log("From merge fn :: ", right);
+    
 
-    let leftIndex= 0;
+    let leftIndex = 0;
     let rightIndex = 0;
 
-    while(leftIndex < left.length && rightIndex < right.length){
-        if(left[leftIndex] < right[rightIndex]){
+    while (leftIndex < left.length && rightIndex < right.length) {
+        console.log('Left Index :: ', leftIndex);
+        console.log('Left value from index :: ', left[leftIndex]);
+
+        console.log('Right Index :: ', rightIndex);
+        console.log('Right value from index :: ', right[rightIndex]);
+        if (left[leftIndex] < right[rightIndex]) {
+
+
+
             result.push(left[leftIndex])
             leftIndex++;
         } else {
             result.push(right[rightIndex])
+
+
             rightIndex++;
         }
     }
-
-
     return [...result, ...left.slice(leftIndex), ...right.slice(rightIndex)];
-
-
-
-    
 }
+
+
+function mergeSort(arr) {
+    if (arr.length <= 1) return arr;
+
+    const mid = Math.floor(arr.length / 2);
+    console.log(mid)
+
+    const left = arr.slice(0, mid);
+    console.log("left ::", left);
+    const right = arr.slice(mid);
+    console.log("Right ::", right);
+
+
+    return merge(mergeSort(left), mergeSort(right));
+}
+
+const numbers = [38, 27, 43, 3, 9, 82, 10];
+
+
+console.log(mergeSort(numbers));
+
+
+// average case --> 0(nlogn)
+// best case --> 0(nlogn)
+// worst case --> 0(nlogn)
+
+// space complexity 0(n)
+
+
